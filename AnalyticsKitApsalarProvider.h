7311563af12d3dfd82ce50219b91faf5fb9225aa
Christopher Pickslay <cpickslay@twobitlabs.com>
2012-06-20 16:07:56 -0700

    added missing import

--- AnalyticsKitApsalarProvider.h
+++ AnalyticsKitApsalarProvider.h
@@ -6,7 +6,7 @@
 //  Copyright (c) 2012 Bleacher Report. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#import "AnalyticsKit.h"
 
 @interface AnalyticsKitApsalarProvider : NSObject <AnalyticsKitProvider>
 
