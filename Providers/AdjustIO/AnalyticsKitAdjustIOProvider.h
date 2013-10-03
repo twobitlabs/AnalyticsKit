00104569d3f03543077cb3afb806b4dd28df6c8d
Brad Bergeron <bradbergeron@gmail.com>
2013-10-03 01:42:27 -0400

    Restoring AdjustIO

---
+++ Providers/AdjustIO/AnalyticsKitAdjustIOProvider.h
@@ -0,0 +1,14 @@
+//
+//  AnalyticsKitAdjustIOProvider.h
+//  S2M
+//
+//  Created by François Benaiteau on 10/29/13.
+//
+
+#import "AnalyticsKit.h"
+
+@interface AnalyticsKitAdjustIOProvider : NSObject<AnalyticsKitProvider>
+
+-(id<AnalyticsKitProvider>)initWithAppToken:(NSString *)appToken;
+-(void)enableProductionEnvironment:(BOOL)enabled;
+@end
